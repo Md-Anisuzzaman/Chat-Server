951bad89571be1af796a34402c8ce5e7ae91374f
Md-Anisuzzman <Anisuzzaman1199@gmail.com>
2022-03-17 11:44:23 +0600

    solve-error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
-const port =process.env.port || 5000;
+const port =process.env.PORT || 5000;
 app.use(cors());
 
 
